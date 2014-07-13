e9e066a67c4b75df12b63dab589bbce08329c0d5
blackchestnut <alexander.kalinichev@gmail.com>
2014-07-13 20:03:55 +0400

    total cards on view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,10 @@
 class ListsController < ApplicationController
   def show
     @board = client.board(params[:board_id])
-    @list = @board.lists.select{ |e| e.id == params[:list_id] }.first
+    if params[:list_id] == 'all'
+      @lists = @board.lists
+    else
+      @lists = @board.lists.select{ |e| e.id == params[:list_id] }
+    end
   end
 end
